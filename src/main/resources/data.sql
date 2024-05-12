4e3cbb7c8492474526e972b3d3da4fc090cc47d1
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-12 11:57:02 -0300

    adicionando arquivo sql para popular tabelas do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO PESSOAS(nome, cpf, nascimento) VALUES('Nelson Machado', '000.111.222-44', '1968-10-04');
+INSERT INTO PESSOAS(nome, cpf, nascimento) VALUES('Ana Raquel', '111.222.333-55', '2004-06-29');
+INSERT INTO PESSOAS(nome, cpf, nascimento) VALUES('Samuel Yamamoto', '222.333.444-44', '2001-08-17');
+
+INSERT INTO ENDERECOS(rua, numero, complemento, bairro, cidade, estado, cep, pessoa_id) VALUES('Rua Mar Del Plata', 878, 'casa', 'Jardim América', 'São Jose dos Campos', 'SP', '12233-045', 1);
+INSERT INTO ENDERECOS(rua, numero, complemento, bairro, cidade, estado, cep, pessoa_id) VALUES('Rua Rui Barbosa', 333, 'apto.57', 'Bom Sucesso', 'Guarulhos', 'SP', '08976-212', 2);
+INSERT INTO ENDERECOS(rua, numero, complemento, bairro, cidade, estado, cep, pessoa_id) VALUES('Avenida Independencia', 1055, 'apto.112', 'Centro', 'Campinas', 'SP', '19222-035', 3);
+INSERT INTO ENDERECOS(rua, numero, complemento, bairro, cidade, estado, cep, pessoa_id) VALUES('Rua Rua Jose Moreno da Silva', 54, 'sem complemento', 'Jardim das Flores', 'Santos', 'SP', '13054-006', 1);

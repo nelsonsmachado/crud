ca5536b66a99d8d794a7c913b8ed6331facf8c9e
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-14 08:38:08 -0300

    metodo exibeEnderecoPorId criado em arquivo EnderecoController.java

--- src/main/java/com/dbserver/crud/controller/EnderecoController.java
+++ src/main/java/com/dbserver/crud/controller/EnderecoController.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/enderecos")
@@ -31,5 +33,11 @@ public class EnderecoController {
         return new ResponseEntity<>(enderecos, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Endereco> exibeEnderecoPorId(@PathVariable Long id) {
+        Optional<Endereco> enderecoPorId = enderecoService.exibeEnderecoPorId(id);
+        return enderecoPorId.map(endereco -> new ResponseEntity<>(endereco, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
 }

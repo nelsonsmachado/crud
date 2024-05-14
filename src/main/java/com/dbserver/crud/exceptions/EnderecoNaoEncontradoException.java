ceb55723cec172172a2c2591ea8f7dd626b875ff
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-14 09:55:38 -0300

    classe EnderecoNaoEncontradoException.java criado no pacote exceptions

---
+++ src/main/java/com/dbserver/crud/exceptions/EnderecoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.dbserver.crud.exceptions;
+
+public class EnderecoNaoEncontradoException extends Throwable {
+    public EnderecoNaoEncontradoException(Long mensagem) {
+        super();
+    }
+}

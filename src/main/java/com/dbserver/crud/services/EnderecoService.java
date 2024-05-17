622e4fcfdd7af45a91d19d229f65e09cd13c788c
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-17 19:21:48 -0300

    metodo modificaEndereco criado em EnderecoService.java

--- src/main/java/com/dbserver/crud/services/EnderecoService.java
+++ src/main/java/com/dbserver/crud/services/EnderecoService.java
@@ -1,10 +1,7 @@
 package com.dbserver.crud.services;
 
 import com.dbserver.crud.entities.Endereco;
-import com.dbserver.crud.entities.Pessoa;
-import com.dbserver.crud.exceptions.CpfExistenteException;
 import com.dbserver.crud.exceptions.EnderecoNaoEncontradoException;
-import com.dbserver.crud.exceptions.UsuarioNaoEncontradoException;
 import com.dbserver.crud.repositories.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,9 +26,26 @@ public class EnderecoService {
         return enderecoRepository.findById(id);
     }
 
+    public Endereco modificaEndereco(Endereco endereco) throws EnderecoNaoEncontradoException {
+        Optional<Endereco> enderecoOptional = enderecoRepository.findById(endereco.getId());
+        if (enderecoOptional.isPresent()) {
+            Endereco enderecoSalvo = enderecoOptional.get();
+            enderecoSalvo.setRua(endereco.getRua());
+            enderecoSalvo.setNumero(endereco.getNumero());
+            enderecoSalvo.setComplemento(endereco.getComplemento());
+            enderecoSalvo.setBairro(endereco.getBairro());
+            enderecoSalvo.setCidade(endereco.getCidade());
+            enderecoSalvo.setEstado(endereco.getEstado());
+            enderecoSalvo.setCep(endereco.getCep());
+            return enderecoRepository.save(enderecoSalvo);
+        } else {
+            throw new EnderecoNaoEncontradoException("Endereço não encontrado pelo ID informado.");
+        }
+    }
+
     public void exclueEndereco(Long id) throws EnderecoNaoEncontradoException {
         Endereco endereco = enderecoRepository.findById(id)
-                .orElseThrow(() -> new EnderecoNaoEncontradoException(id));
+                .orElseThrow(() -> new EnderecoNaoEncontradoException("Endereço não encontrado com o ID: " + id.toString()));
         enderecoRepository.delete(endereco);
     }
 }

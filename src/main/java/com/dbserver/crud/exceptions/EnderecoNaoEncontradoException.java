bd405b90270f3d3cd3e25d7a1f9ef70d1d84892b
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-18 22:41:35 -0300

    class EnderecoNaoEcontradoException.java alterado para adequação após alteração de EnderecoController.java

--- src/main/java/com/dbserver/crud/exceptions/EnderecoNaoEncontradoException.java
+++ src/main/java/com/dbserver/crud/exceptions/EnderecoNaoEncontradoException.java
@@ -1,7 +1,8 @@
 package com.dbserver.crud.exceptions;
 
-public class EnderecoNaoEncontradoException extends Exception {
-    public EnderecoNaoEncontradoException(String mensagem) {
-        super(mensagem);
+public class EnderecoNaoEncontradoException extends RuntimeException {
+    public EnderecoNaoEncontradoException(Long id) {
+        super("Endereço não encontrado com o ID: " + id);
     }
 }
+

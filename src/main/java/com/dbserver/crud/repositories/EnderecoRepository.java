f9127c1c3a9be27c1dfc4bb9661b7b0a2d343d77
Nelson Machado <nelson.machado@dbserver.com.br>
2024-05-18 23:03:13 -0300

    refatorado para adequacao após modificação do arquivo EnderecoController.java

--- src/main/java/com/dbserver/crud/repositories/EnderecoRepository.java
+++ src/main/java/com/dbserver/crud/repositories/EnderecoRepository.java
@@ -6,5 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    boolean existsByRuaAndNumeroAndComplementoAndBairroAndCidadeAndEstadoAndCep(String rua, int numero, String complemento, String bairro, String cidade, String estado, String cep);
 
 }
+
+
